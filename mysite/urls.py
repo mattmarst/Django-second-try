2f05dbffb521b7c25cb9b278091bfc8d5128b260
Matt <mattmarston110@gmail.com>
2023-10-08 10:58:39 -0400

    Auto redirect to Polls app when opened

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
+    #path("", views.index, name="index"),
+    #path('', RedirectView.as_view(url='/polls')),
+    path('', RedirectView.as_view(pattern_name='polls:index')),
     path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
 ]
