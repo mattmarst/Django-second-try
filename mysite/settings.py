a5d5520e21e3773ca41ecba734ef50bca6400faa
mmarston110 <mattmarston110@gmail.com>
2023-10-12 21:23:19 -0400

    Transistioning to postgres database

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,10 +11,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+#SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG')
 
 
 # Quick-start development settings - unsuitable for production
@@ -78,8 +82,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': '5432',
     }
 }
 
